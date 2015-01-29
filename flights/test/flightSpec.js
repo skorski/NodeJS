040866d04ddd1c665e2b9379ce2c090270097f79
Dan Skorski <skorsk@gmail.com>
2015-01-29 00:16:33 -0500

    added db connection to mongo-lab.  finished unit test portion.

---
+++ flights/test/flightSpec.js
@@ -0,0 +1,63 @@
+var app = require('./helpers/app');
+
+var should = require('should'),
+	supertest = require('supertest');
+
+describe('flights', function () {
+
+	it('should pass', function (done) {
+		done();
+	});
+
+	it('should return valid flight data for flight 18', function (done) {
+		supertest(app)
+		.get('/flight/18')
+		.expect(200)
+		.end(function (err, res){
+			res.status.should.equal(200);
+			done();
+		});
+
+	});
+
+
+	it('should return an error for an invlid flight', function(done) {
+		supertest(app)
+		.get('/flight/41499999')
+		.expect(404)
+		.end(function (err, res){
+			res.status.should.equal(404);
+			done();
+		});
+
+	}); 
+
+
+	it('should mark a flight as arrived', function (done) {
+		supertest(app)
+		.put('/flight/18/arrived')
+		.expect(200)
+		.end(function (err,res) {
+			res.status.should.equal(200);
+			res.body.status.should.equal('done');
+
+			supertest(app)
+			.get('/flight/18/')
+			.expect(200)
+			.end(function (err, res) {
+				res.status.should.equal(200);
+				res.body.actualArrive.should.not.equal(undefined);
+				done();
+			});
+
+		});
+
+	});
+
+
+	it('should not pass', function (done) {
+		throw "don't pass";
+		done();
+	});
+
+});
